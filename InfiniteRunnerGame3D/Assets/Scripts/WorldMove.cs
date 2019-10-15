586fbaff8ad729fa78b22e439382911feaa81e8e
John Schlitters <jschlitters@gmail.com>
2019-10-15 15:54:24 -0600

    More Initial Items
    Basic Concept for Runner Game

---
+++ InfiniteRunnerGame3D/Assets/Scripts/WorldMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("starting world movement");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
