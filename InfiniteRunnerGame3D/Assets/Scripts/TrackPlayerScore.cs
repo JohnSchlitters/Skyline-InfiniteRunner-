e11ab20fbad433cf10424a0cf91feb61ff4b7d0a
John Schlitters <jschlitters@gmail.com>
2019-10-19 23:54:58 -0600

    Score Start
    Started working on UI and script elements for scorekeeping

---
+++ InfiniteRunnerGame3D/Assets/Scripts/TrackPlayerScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackPlayerScore : MonoBehaviour
+{
+    public float playerScore = 0f;
+    public WorldMove getScoreMultiplier ;
+
+    public Text uiPlayerScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Initialize player score tracking");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerScore = (playerScore * getScoreMultiplier.speedModifier);
+        uiPlayerScore.text = (playerScore.ToString());
+    }
+}
